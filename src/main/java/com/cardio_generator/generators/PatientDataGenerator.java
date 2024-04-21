fd388f70855b7f8fbd5009433157b35ace4f6ce9
Horfi <103373320+Horfi@users.noreply.github.com>
2024-04-22 00:00:31 +0200

    Update PatientDataGenerator.java

--- src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
+++ src/main/java/com/cardio_generator/generators/PatientDataGenerator.java
@@ -1,7 +1,14 @@
 package com.cardio_generator.generators;
 
 import com.cardio_generator.outputs.OutputStrategy;
-
+/**
+ * Defines how to cook up some fake patient data.
+ * Anything that wants to pretend to be a data gen needs to follow this blueprint.
+ */
 public interface PatientDataGenerator {
+    /**
+         * @param patientId The ID of the patient – gotta know who we're talking about.
+     * @param outputStrategy The method we're gonna use to get this data out there.
+     */
     void generate(int patientId, OutputStrategy outputStrategy);
 }
